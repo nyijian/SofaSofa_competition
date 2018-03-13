76861d46d1f88ae9a3d570aadcc1d37761c82a6f
yang <yang535460548484@gmail.com>
2018-03-13 23:52:08 +0800

    add data process function

--- player_price_predict/data.py
+++ player_price_predict/data.py
@@ -1,14 +1,20 @@
 import csv
 import numpy as np
+from sklearn.preprocessing import StandardScaler
+import pickle
 
+#需要进行特殊处理的features
 club_index = 1
 league_index = 2
 birth_date_index = 3
 nationality_index = 6
-weak_foot_index = 16
 work_rate_att_index = 17
 work_rate_def_index = 18
 preferred_foot_index = 19
+
+speciel_index = [club_index,league_index,birth_date_index,nationality_index,work_rate_att_index,work_rate_def_index,preferred_foot_index]
+
+#加载csv文件数据
 def load_data(filepath):
     data = []
     with open(filepath) as f:
@@ -16,14 +22,34 @@ def load_data(filepath):
         next(reader)
         for row in reader:
             data.append(row)
-    return np.array[data]
+    data = np.array(data)
+    #把空字符串替换为0
+    data[data==""]=0
+    return data
 
-# def preprocess(data):
+def data_scaler_train(data):
+    scaler = StandardScaler()
+    data_scaled = scaler.fit_transform(data)
+    #保存scaler，以便处理test数据集时调用
+    output = open('scaler.pkl', 'wb')
+    pickle.dump(scaler,output)
+    return data_scaled
+
+def data_scaler_test(data):
+    #用pickle加载scaler
+    input = open('scaler.pkl', 'rb')
+    scaler = pickle.load(input)
+    data_scaled = scaler.transform(data)
+    return data_scaled
+
+def preprocess(data):
+    #筛选出不需要进行特殊处理的features
+    data_normal = np.delete(data,speciel_index,axis=1)
+    data_normal_scaled = data_scaler_train(data_normal)
+
+
 if __name__ == "__main__":
-    with open("./data/train.csv") as f:
-        reader = csv.reader(f)
-        next(reader)
-        print(next(reader))
-        a = np.array(next(reader))
-        print(a)
-        print (a[1]/a[2])
+    filepath = "./data/train.csv"
+    data = load_data(filepath)
+    preprocess(data)
+
